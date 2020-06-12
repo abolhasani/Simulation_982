556005749699ff417da1aa2797abbd386ff60c95
Mohammad Sadegh Abolhasani <abolhasani@users.noreply.github.com>
2020-06-12 11:39:17 +0430

    Update Simulation_Phase2_Total.py

--- Simulation_Phase2_Total.py
+++ Simulation_Phase2_Total.py
@@ -1,1 +1,20 @@
+"""
+Simulation of fast-food restaurant
+system description should be added
+Outputs : 1-mean time for customer being in the system
+          2-mean customer's waiting time in receiving food
+          3-mean and maximum of queue length in serving food part
+          4-mean performance of the servers in ordering and receiving part
+          5-mean number of customers in the system
+Starting State = ...
 
+Authors: Mohammad Sadegh Abolhasani, Abolfazl Tghavi
+Starting Date:9 May 2020
+Finishing Date:... May 2020
+"""
+
+import random
+import math
+import pandas as pd
+###import numpy as np
+from operator import itemgetter
