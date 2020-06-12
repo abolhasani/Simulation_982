56e1ec719dbd63760db47b26d74ed1854f949638
abolfaztaghavi <63851743+abolfaztaghavi@users.noreply.github.com>
2020-06-12 11:11:14 +0400

    Update Simulation_Phase2_Total.py

--- Simulation_Phase2_Total.py
+++ Simulation_Phase2_Total.py
@@ -18,3 +18,96 @@ import math
 import pandas as pd
 ###import numpy as np
 from operator import itemgetter
+# an array for holding the information of all customers attended
+customers = []
+# defining statistics which should be updated during the simulation
+total_time_customer_in_system = 0
+total_num_of_exited_customers = 0
+total_time_customer_in_receiving_queue = 0
+total_num_of_customers_received_food = 0
+serving_food_queue_length = []
+total_ordering_server_busy_time = 0
+total_receiving_server_busy_time = 0
+ordering_servers_rest_time = 0
+receiving_servers_rest_time = 0
+
+
+# defining a class for customers
+class Customer:
+    index = 0
+    entering_time = 0
+    exit_time = 0
+    entering_time_to_receiving_section = 0
+
+    def __init__(self, index, enetering_time):
+        self.index = index
+        self.entering_time = enetering_time
+
+
+# initialization of the system states and FEL
+def starting_state():
+
+    # this clock will be used in the code and it is based on the minutes which is passed till then
+    clock = 0
+    # this clock has two part: one for hour and the other for minutes and it will start from 10 AM
+    Real_clock = dict()
+    Real_clock["hour"] = 10
+    Real_clock["minute"] = 0
+
+    step = 0
+
+    # State
+    state = dict()
+    state['Ordering_Server_Idle'] = 5
+    state['Ordering_Server_Resting'] = 0
+    state['Ordering_Server_Rest_blocked'] = 0
+    state['Ordering_queue'] = 0
+    state['Receiving_Server_Idle'] = 2
+    state['Receiving_Server_Resting'] = 0
+    state['Receiving_Server_Rest_blocked'] = 0
+    state['Receiving_queue'] = 0
+    state['Serving_Chairs_Idle'] = 30
+    state['serving_queue'] = 0
+
+    # the initial FEL
+    future_event_list = list()
+    FEL_maker(future_event_list, ["Event type" , "Event time" , "Customer index"], ["PCE" , exponential_random_variate(3) , 0])
+
+    return state, future_event_list, step, clock, Real_clock
+
+
+# a function for making new event notices and adding them to the FEL
+def FEL_maker(future_event_list,keys, values):
+    new_event = dict()
+    for i in range(len(keys)):
+        new_event[keys[i]] = values[i]
+    future_event_list.append(new_event)
+
+# should be developed by Abolfazl
+def triangular_random_variate(a , b , c , R):
+    def triangular_random_variate(a, b, c , R):
+    h = (c-a)/2
+    threshold = (b-a)/(c-a)
+    if R < threshold :
+        return (np.sqrt((c-a)*(b-a)*R) + a)
+    else :
+        x = c-a
+        y = c-b
+        return (c - np.sqrt((1-R)*x*y))
+
+
+# should be developed by Abolfazl
+def poisson_random_variate(mean):
+    return 0
+
+
+# should be developed by Abolfazl
+def num_of_car_passangers(R):
+    if R < 0.2 :
+        return 1
+    elif R < 0.5 :
+        return 2
+    elif R < 0.8 :
+        return 3
+    else :
+        return 4
